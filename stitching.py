d7588b284991cc6315a0fa632080fa36857dea13
GY_Quantic <1357363304@qq.com>
2020-06-21 22:18:54 +0800

    new version with full function

--- stitching.py
+++ stitching.py
@@ -4,11 +4,12 @@ import random
 import math
 import numpy as np
 from copy import deepcopy
-from copy import copy
+from sklearn.linear_model import LinearRegression
 
+dir_path = ''
 feature_points_num = 10
 threshold = 3
-matrix_size = 3
+matrix_size = 30
 window_size = 3
 LEFT_BASE = 0
 RIGTH_BASE = 1
@@ -78,7 +79,7 @@ def ranSAC(matches, kp1, kp2):
     """
     H_best = None
     I_most = 0
-    iter_times = int(math.log(0.01) / math.log(1 - pow(0.9, feature_points_num)))
+    iter_times = int(math.log(0.01) / math.log(1 - pow(0.8, feature_points_num)))
     threshold = 3
     best_inliners = []
     for k in range(iter_times):
@@ -98,85 +99,54 @@ def ranSAC(matches, kp1, kp2):
             I_most = len(inliners)
             best_inliners = inliners
     return H_best, best_inliners
-"""
+
 # version2
-def fillIndex(start, end, index):
-    if start+2>end:
+def fillIndex(start, end, index, base):
+    if start + 2 > end:
         return
-    if index[start] == index[end]:
-        mid = int((start + end) / 2)
-        index[mid] = 1 - index[start]
-        step = (index[mid] - index[start]) / (mid - start)
-        for i in range(start + 1, mid):
-            index[i] = index[i - 1] + step
-        step = (index[end] - index[mid]) / (end - mid)
-        for i in range(mid + 1, end):
-            index[i] = index[i - 1] + step
-    else:
-        step = index[end] - index[start] / (end - start)
-        for i in range(start + 1, end):
-            index[i] = index[i - 1] + step
-def getBlending(img_raw, img_result,aaaaa):
-    rows, cols = img_result.shape[:2]
-    result = np.zeros([rows, cols, 3], np.uint8)
-    for row in range(0, rows):
-        # make a row_index for each line with marks
-        col_index = []
-        for col in range(0, cols):
-            #  img_raw \ img_result |     0       1
-            #  ------------------------------------------------------
-            #        0              |     -1      0
-            #        1              |     1       0.5
-            if col >= img_raw.shape[1] or not img_raw[row, col].any(): # 旧图无
-                if img_result[row, col].any(): # 新图有
-                    col_index.append(0)
-                else:
-                    col_index.append(1)
-            else:
-                if img_result[row, col].any(): # 新图有
-                    col_index.append(0.5)
-                else:
-                    col_index.append(0)
-        col = 0
-        while (col < cols):
-            if col_index[col] == 0.5:
-                start = col 
-                while(col < cols and col_index[col] == 0.5):
-                    col += 1
-                end = col-1
-                fillIndex(start, end, col_index)
-            else:
-                col += 1
-        for col in range(0, cols):
-            if col_index[col] == -1:
-                result[row, col] = np.array([0, 0, 0])
-            elif col >= img_raw.shape[1]:
-                result[row, col] = img_result[row, col]
-            else:
-                alpha = col_index[col]
-                result[row, col] = np.clip(img_raw[row, col] * alpha + img_result[row, col] * (1 - alpha), 0, 255)
-    left, right = 0, 0
-    for col in range(0, result.shape[:2][1]):
-        if result[:, col].any():  # 合成图的最左端
+    step = (0.5 - base) * 2 / (end - start)
+    for i in range(start + 1, end):
+        index[i] = index[i - 1] + step
+
+def getBlending(img_raw, img_result, base):
+    for col in range(0, img_result.shape[:2][1]):
+        if img_result[:, col].any() or img_raw[:, col].any():  # 合成图的最左端
             left = col
             break
-    for col in range(result.shape[:2][1] - 1, 0, -1):
-        if result[:, col].any():  # 合成图的最右端
+    for col in range(img_result.shape[:2][1] - 1, 0, -1):
+        if img_result[:, col].any() or img_raw[:, col].any():  # 合成图的最右端
             right = col
             break
-    for row in range(0, result.shape[:2][0]):
-        if result[row, :].any():  # 合成图的最左端
+    for row in range(0, img_result.shape[:2][0]):
+        if img_result[row, :].any() or img_raw[row, :].any():  # 合成图的最左端
             top = row
             break
-    for row in range(result.shape[:2][0] - 1, 0, -1):
-        if result[row, :].any():  # 合成图的最左端
-            bottom = col
+    for row in range(img_result.shape[:2][0] - 1, 0, -1):
+        if img_result[row, :].any() or img_raw[row, :].any():  # 合成图的最右端
+            bottom = row
             break
-    result = result[top:bottom, left: right]
+    img_result = img_result[top: bottom, left: right]
+    img_raw = img_raw[top: bottom, left: right]
+    rows, cols = img_result.shape[:2]
+    result = np.zeros([rows, cols, 3], np.uint8)
+    train_x = []
+    train_y = []
+    for row in range(0, rows):
+        for col in range(0, cols):
+            if img_result[row, col].any() and img_raw[row, col].any():
+                train_x.append(img_result[row, col])
+                train_y.append(img_raw[row, col])
+    clf = LinearRegression()
+    clf.fit(train_x, train_y)
+    for row in range(0, rows):
+        for col in range(0, cols):
+            if img_result[row, col].any():
+                result[row, col] = np.clip(clf.predict([img_result[row, col]])[0], 0, 255)
+            elif img_raw[row, col].any():
+                result[row, col] = img_raw[row, col]
     return result
 """
 # version1
-
 def judgeBlack(row, col, img):
     global matrix_size, avg_points
     if col <= matrix_size:
@@ -271,6 +241,24 @@ def extendList(points, res):
     return list(set(result))
 
 def getBlending(img_raw, img_result, base):
+    for col in range(0, img_result.shape[:2][1]):
+        if img_result[:, col].any() or img_raw[:, col].any():  # 合成图的最左端
+            left = col
+            break
+    for col in range(img_result.shape[:2][1] - 1, 0, -1):
+        if img_result[:, col].any() or img_raw[:, col].any():  # 合成图的最右端
+            right = col
+            break
+    for row in range(0, img_result.shape[:2][0]):
+        if img_result[row, :].any() or img_raw[row, :].any():  # 合成图的最左端
+            top = row
+            break
+    for row in range(img_result.shape[:2][0] - 1, 0, -1):
+        if img_result[row, :].any() or img_raw[row, :].any():  # 合成图的最右端
+            bottom = row
+            break
+    img_result = img_result[top: bottom, left: right]
+    img_raw = img_raw[top: bottom, left: right]
     rows, cols = img_raw.shape[:2]
     for col in range(0, cols):
         if img_raw[:, col].any() and img_result[:, col].any():  # 开始重叠的最左端
@@ -281,7 +269,6 @@ def getBlending(img_raw, img_result, base):
             right = col
             break
     res = np.zeros([rows, cols, 3], np.uint8)
-    avg_points = []
     for row in range(0, rows):
         for col in range(0, cols):
             srcImgLen = float(abs(col - left))
@@ -292,39 +279,27 @@ def getBlending(img_raw, img_result, base):
             elif not img_result[row, col].any():
                 res[row, col] = img_raw[row, col]
             else:
-                if judgeBlack(row, col, img_result):
-                    avg_points.append((row, col))
-                    res[row, col] = img_raw[row, col]
-                    continue
-                # cv2.circle(img1111, (col, row), 1, (0, 255, 0), -1)
                 if base == RIGTH_BASE:
                     res[row, col] = np.clip(img_raw[row, col] * alpha + img_result[row, col] * (1 - alpha), 0, 255)
                 else:
                     res[row, col] = np.clip(img_raw[row, col] * (1 - alpha) + img_result[row, col] * alpha, 0, 255)
+
     # print(len(avg_points))
     # avg_points = extendList(avg_points, img_result)
     # print(len(avg_points))
     img_result[0:img_raw.shape[0], 0:img_raw.shape[1]] = res
+    # cv2.imwrite("0.jpg", img_result)
     # for point in avg_points:
     #    img_result[point[0], point[1]] = getAvg(point, img_result)
-    for col in range(0, img_result.shape[:2][1]):
-        if img_result[:, col].any():  # 合成图的最左端
-            left = col
-            break
-    for col in range(img_result.shape[:2][1] - 1, 0, -1):
-        if img_result[:, col].any():  # 合成图的最右端
-            right = col
-            break
-    img_result = img_result[:, left: right]
     return img_result
+"""
+def contain(shape_range, u, v):
+    return u >= 0 and u < shape_range[0] - 1 and v >= 0 and v < shape_range[1] - 1
 
-def contain(src, u, v):
-    return u>=0 and u<src.shape[0]-1 and v>=0 and v<src.shape[1]-1
-
-def pointSampling(src, u, v):
+def pointSampling(src, u, v, shape_range):
     u = int(u+0.5)
     v = int(v+0.5)
-    if not contain(src, u, v):
+    if not contain(shape_range, u, v):
         return deepcopy(src[0,0])
     return deepcopy(src[u,v])
 
@@ -337,23 +312,19 @@ def toInt(a):
 def bilinear(a, b, w):
     a = toFloat(a)
     b = toFloat(b)
-    return a*w + b*(1-w);
+    return b * w + a * (1 - w)
 
 def triangleFiltering(src, u, v):
     x = [int(u),int(u+1)]
     y = [int(v),int(v+1)]
-    a = bilinear(src[x[0],y[0]], src[x[0],y[1]], u-x[0])
-    b = bilinear(src[x[1],y[0]], src[x[1],y[1]], u-x[0])
+    a = bilinear(src[x[0],y[0]], src[x[0],y[1]], v-y[0])
+    b = bilinear(src[x[1],y[0]], src[x[1],y[1]], v-y[0])
     # print (a,b)
-    return toInt(bilinear(a, b, v-y[0]))
+    return toInt(bilinear(a, b, u-x[0]))
 
-def warp(src, H, shape):
+def warp(src, H, shape, shape_range):
     H = np.linalg.inv(H)
     dst = np.ndarray((shape[0],shape[1],3),dtype=np.uint8)
-    if H.shape != (3,3):
-        print ("wrong homnography")
-        print (H)
-        exit(0)
     for x in range(shape[1]):
         for y in range(shape[0]):
             v = (H[0,0]*x + H[0,1]*y + H[0,2]) / (H[2,0]*x + H[2,1]*y + H[2,2])
@@ -361,9 +332,9 @@ def warp(src, H, shape):
             # t = H.dot(np.array([x,y,1]))
             # t = t / t[2]
             # v, u = t[0], t[1]
-            if contain(src, u, v):
-                dst[y,x] = pointSampling(src, u, v)
-                #dst[y,x] = triangleFiltering(src, u, v)
+            if contain(shape_range, u, v):
+                # dst[y,x] = pointSampling(src, u, v, shape_range)
+                dst[y,x] = triangleFiltering(src, u, v)
     return dst
 
 def count(a):
@@ -373,49 +344,29 @@ def count(a):
 
 def combineImg(img1, img2, base):
     global sift, match, avg_points
-
-    # kp1, des1 = sift.detectAndCompute(img1, None)
-    # kp2, des2 = sift.detectAndCompute(img2, None)
-    # matches = matchFeatures(des1, des2)
-    # H, inliners = ranSAC(matches, kp1, kp2)
-    # img_result = cv2.warpPerspective(img2, H, (img1.shape[1], img1.shape[0]))
-    # cv2.imshow('1', img_result)
-    # cv2.waitKey(0)
-    # exit(0)
-    
-    # if base == RIGTH_BASE:
-        # img1 = cv2.copyMakeBorder(img1, 0, 0, img2.shape[1], 0, cv2.BORDER_CONSTANT, value=0)
-    img1 = cv2.copyMakeBorder(img1, img1.shape[1], img1.shape[0], img1.shape[1], img1.shape[0], cv2.BORDER_CONSTANT, value=0)
-    img2 = cv2.copyMakeBorder(img2, img1.shape[1], img1.shape[0], img1.shape[1], img1.shape[0], cv2.BORDER_CONSTANT, value=0)
+    img1 = cv2.copyMakeBorder(img1, img2.shape[1], img2.shape[0], img2.shape[1], img2.shape[0], cv2.BORDER_CONSTANT, value=0)
+    # img2 = cv2.copyMakeBorder(img2, img1.shape[1], img1.shape[0], img2.shape[1], img2.shape[0], cv2.BORDER_CONSTANT, value=0)
     kp1, des1 = sift.detectAndCompute(img1, None)
     kp2, des2 = sift.detectAndCompute(img2, None)
     matches = matchFeatures(des1, des2)
     H, inliners = ranSAC(matches, kp1, kp2)
-    # matches = matchFeatures(des2, des1)
-    # H, inliners = ranSAC(matches, kp2, kp1)
-    print (H)
+    print(H)
     H = np.asarray(H)
-    # if base == RIGTH_BASE:
-        # img_result = cv2.warpPerspective(img2, H, (img1.shape[1], img1.shape[0]))
-    img_result = warp(img2, H, (3*img1.shape[0], 3*img1.shape[1]))
-    # else:
-        # img_result = cv2.warpPerspective(img2, H, (img1.shape[1] + img2.shape[1], img1.shape[0]))
-    # img_result = warp(img2, H, (img1.shape[0], img1.shape[1] + img2.shape[1]))
-
-    # cv2.imshow('1', img_result)
-    # cv2.waitKey(0)
-    # exit(0)
+    img_result = warp(img2, H, img1.shape, img2.shape)
     img_result = getBlending(img1, img_result, base)
     return img_result
 
 def getImgs():
+    global dir_path
     imgs = []
     base = 'source'
     dir_path = input('Please input file name:')
-    dir_path = os.path.join(base,dir_path)
-    files = os.listdir(dir_path)
+    if dir_path == '':
+        dir_path = 'source008'
+    complete_path = os.path.join(base,dir_path)
+    files = os.listdir(complete_path)
     for file in files:
-        imgs.append(cv2.imread(os.path.join(dir_path, file)))
+        imgs.append(cv2.imread(os.path.join(complete_path, file)))
     return imgs
 
 if __name__ == "__main__":
@@ -424,6 +375,7 @@ if __name__ == "__main__":
     imgs = getImgs()
     # 从中央开始合成图片
     img_num = len(imgs)
+    print("result/" + dir_path + ".jpg")
     if img_num % 2 == 1:
         base_img = imgs[int(img_num / 2)]
         for i in range(int(img_num / 2)):
@@ -441,8 +393,9 @@ if __name__ == "__main__":
     for i in range(2, len(imgs)):
         base_img = combineImg(base_img, imgs[i], LEFT_BASE)
     """
-    cv2.imshow("original_image_drawMatches.jpg", base_img)
-    cv2.waitKey(0)
+    cv2.imwrite("111.jpg", base_img)
+    # cv2.imwrite("result/"+ dir_path + ".jpg", base_img)
+    # cv2.waitKey(0)
 #    cv2.imwrite(dir_path + ".jpg", base_img)
 
 #draw_params = dict(matchColor = (0,255,0), # draw matches in green color
